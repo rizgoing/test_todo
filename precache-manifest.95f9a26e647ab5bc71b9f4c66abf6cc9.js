b7708fda091179b2c573f7bdf51289a21231cec8
rizgoing <slvmdev66@gmail.com>
2020-06-14 14:24:27 +0300

    Updates

---
+++ precache-manifest.95f9a26e647ab5bc71b9f4c66abf6cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388ef646d1cda0fd6bbc1afeb94c167a",
+    "url": "/test-todo/index.html"
+  },
+  {
+    "revision": "1a087303a07d3ecc4181",
+    "url": "/test-todo/static/css/main.fa472301.chunk.css"
+  },
+  {
+    "revision": "11302595c15be496311b",
+    "url": "/test-todo/static/js/2.e802cefa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-todo/static/js/2.e802cefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a087303a07d3ecc4181",
+    "url": "/test-todo/static/js/main.fac75d35.chunk.js"
+  },
+  {
+    "revision": "07ee7040deba3161c154",
+    "url": "/test-todo/static/js/runtime-main.7a745fe2.js"
+  }
+]);
